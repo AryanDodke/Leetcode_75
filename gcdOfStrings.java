d5c5feb9bff6a5053a9847389cf383bf21481eef
AryanDodke <aryandodke2003@gmail.com>
2024-11-12 23:36:45 +0530

    12/11/2024 Leetcode 75 Second Problem

--- gcdOfStrings.java
+++ gcdOfStrings.java
@@ -1,13 +1,18 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        StringBuilder builder = new StringBuilder();
-        int i = 0;
-            if (str1.charAt(i) == str2.charAt(i)) {
-                builder.append(str2);
-            } else {
-                return "";
-            }
-            i++;
-        return builder.toString();
+       int c = str1.length();
+       int d = str2.length();
+       if (!(str1 + str2).equals(str2 + str1)) {
+        return "";
+       }
+       String result = str1.substring(0, gcd(c, d));
+       return result;
+    }
+
+    private int gcd(int a, int b){
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
     }
 }
